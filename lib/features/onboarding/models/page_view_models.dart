100025665425695fe1d9dccce0d74ba01e26ca50
KerolesHany <keroleshany293@gmail.com>
2026-04-19 17:24:58 +0200

    change on boarding images

--- lib/features/onboarding/models/page_view_models.dart
+++ lib/features/onboarding/models/page_view_models.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery3/core/utils/theme/app_colors.dart';
 import 'package:grocery3/core/utils/theme/app_fonts.dart';
 import 'package:grocery3/core/utils/theme/app_styles.dart';
 import 'package:introduction_screen/introduction_screen.dart';
@@ -6,18 +7,13 @@ import 'package:introduction_screen/introduction_screen.dart';
 final List<PageViewModel> pagesViewModel = [
   getCustomPageViewModel(
     title: "Shop Everything You Need, In One Click",
-    imagePath: "assets/images/on_boarding_1.png",
+    imagePath: "assets/images/onboarding_1.png",
     body:
         " Discover over 1000 fresh and essential grocery products in a single place.",
   ),
   getCustomPageViewModel(
     title: "Save Time, Get Groceries Delivered to Your Door",
-    imagePath: "assets/images/on_boarding_2.png",
-    body: "Ultra-fast delivery with real-time tracking straight to you.",
-  ),
-  getCustomPageViewModel(
-    title: "Save Time, Get Groceries Delivered to Your Door",
-    imagePath: "assets/images/on_boarding_3.png",
+    imagePath: "assets/images/onboarding_2.png",
     body: "Ultra-fast delivery with real-time tracking straight to you.",
   ),
 ];
@@ -31,15 +27,15 @@ PageViewModel getCustomPageViewModel({
     decoration: const PageDecoration(imageFlex: 8, bodyFlex: 5),
     image: Stack(
       children: [
-        Container(
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/on_boarding_bg.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
+        // Container(
+        //   width: double.infinity,
+        //   decoration: const BoxDecoration(
+        //     image: DecorationImage(
+        //       image: AssetImage("assets/images/on_boarding_bg.png"),
+        //       fit: BoxFit.cover,
+        //     ),
+        //   ),
+        // ),
         Align(
           alignment: AlignmentGeometry.bottomCenter,
           child: Image.asset(imagePath, height: 252, fit: BoxFit.cover),
@@ -50,7 +46,10 @@ PageViewModel getCustomPageViewModel({
             onPressed: () {
               // navigate to home screen
             },
-            child: Text("Skip", style: AppStyles.font16Bold),
+            child: Text(
+              "Skip",
+              style: AppStyles.font16Bold.copyWith(color: AppColors.darkBlue),
+            ),
           ),
         ),
       ],
